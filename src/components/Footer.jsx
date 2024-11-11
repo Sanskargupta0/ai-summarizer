4b53da776df11c328bbb9980f43a4b71346ddd5e
Sanskargupta0 <sanskar362002@gmail.com>
2024-11-11 12:43:33 +0530

    Init:- ai-summarizer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className='text-white relative z-10 text-center pb-2'>
+      <small>
+        Created by{' '}
+        <a
+          href='https://Sanskarguptadev.vercel.app/'
+          target='_blank'
+          rel='noreferrer'
+          className='hover:underline'
+        >
+          Sanskar
+        </a>
+      </small>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+import { logo } from '../assets';
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <div className='flex items-center gap-2'>
+          <img
+            src={logo}
+            alt='summarizer logo'
+            className='h-9 object-contain'
+          />
+          <p className='text-3xl text-white font-extrabold light_mode'>Summarizer</p>
+        </div>
+
+        <button
+          type='button'
+          onClick={() => window.open('https://github.com/Sanskargupta0')}
+          className='black_btn'
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='purple_gradient'>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Experience effortless reading with AI Summarizer, a powerful open-source
+        tool that condenses lengthy articles into concise summaries.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
